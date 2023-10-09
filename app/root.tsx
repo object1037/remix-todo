4aaad039b9f3e20942e14e4b39542428b8c3babf
object1037 <contact@object1037.dev>
2023-10-09 17:02:08 +0900

    use wrangler's live-reload option instead of Remix LiveReload
    動かなかったので

--- app/root.tsx
+++ app/root.tsx
@@ -2,16 +2,15 @@ import type { LinksFunction } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+]
 
 export default function App() {
   return (
@@ -26,8 +25,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
-  );
+  )
 }
